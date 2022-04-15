90808359c957eac42f0fa2b7f6e8cbfa9e628281
Andrew Glaze <arg60263@uga.edu>
2022-04-15 14:05:55 -0400

    Added Search Bar and Status Label

--- src/main/java/cs1302/gallery/GalleryApp.java
+++ src/main/java/cs1302/gallery/GalleryApp.java
@@ -5,8 +5,12 @@ import java.net.http.HttpClient;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.TilePane;
+import javafx.scene.control.Label;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.geometry.Pos;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -29,7 +33,11 @@ public class GalleryApp extends Application {
 
     private Stage stage;
     private Scene scene;
-    private HBox root;
+    private VBox root;
+    private HBox searchBarHbox;
+    private Label statusLabel;
+    private TilePane imageTilePane;
+    private HBox progressHbox;
 
     /**
      * Constructs a {@code GalleryApp} object}.
@@ -37,14 +45,21 @@ public class GalleryApp extends Application {
     public GalleryApp() {
         this.stage = null;
         this.scene = null;
-        this.root = new HBox();
+        this.root = new VBox(8);
+        this.searchBarHbox = new SearchBarHBox();
+        this.statusLabel = new Label("Type in a term, select a media type, and click the button.");
+        this.imageTilePane = null;
+        this.progressHbox = null;
     } // GalleryApp
 
     /** {@inheritDoc} */
     @Override
     public void init() {
-        // feel free to modify this method
         System.out.println("init() called");
+
+        root.setAlignment(Pos.CENTER_LEFT);
+        root.getChildren().addAll(this.searchBarHbox, this.statusLabel);
+
     } // init
 
     /** {@inheritDoc} */
---
+++ src/main/java/cs1302/gallery/SearchBarHBox.java
@@ -0,0 +1,47 @@
+package cs1302.gallery;
+
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+
+/**
+ * A Node containing the search bar in the iTunes gallery app.
+ */
+public class SearchBarHBox extends HBox {
+    private Button playButton;
+    private Label searchLabel;
+    private TextField searchTextField;
+    private ComboBox<String> mediaTypeComboBox;
+    private Button searchButton;
+
+    /**
+     * Constructs a new {@code SearchBarHBox}.
+     */
+    public SearchBarHBox() {
+        super(8);
+
+        playButton = new Button("Play");
+        searchLabel = new Label("Search:");
+        searchTextField = new TextField("nanahira");
+        mediaTypeComboBox = new ComboBox<String>();
+        searchButton = new Button("Get Images");
+        init();
+    }
+
+    /**
+     * Aranges the UI.
+     */
+    public void init() {
+        this.setAlignment(Pos.CENTER_LEFT);
+        playButton.setDisable(true);
+        mediaTypeComboBox.getItems().addAll("movie", "podcast", "music", "musicVideo", "audiobook",
+            "shortFilm", "tvShow", "software", "ebook", "all");
+        mediaTypeComboBox.setValue("music");
+        this.getChildren().addAll(playButton, searchLabel, searchTextField,
+            mediaTypeComboBox, searchButton);
+
+    }
+}
